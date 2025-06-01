995c7642ba867e918ad45cf094bac52441c97a0b
Christian Mamani <christianmamani.dam@gmail.com>
2025-06-01 16:08:42 +0200

    update before delete from local

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -8,12 +8,11 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Componentes'),
-        backgroundColor: Colors.blueAccent,
-      ),
-      body: _lista(),
-    );
+        appBar: AppBar(
+          title: Text('Componentes'),
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: _lista());
   }
 
   Widget _lista() {
@@ -21,15 +20,12 @@ class HomePage extends StatelessWidget {
       future: menuProvider.cargarData(),
       builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(
-              child: CircularProgressIndicator());
+          return Center(child: CircularProgressIndicator());
         } else if (snapshot.hasError) {
-          return Center(
-              child: Text('Error: ${snapshot.error}'));
+          return Center(child: Text('Error: ${snapshot.error}'));
         } else {
           return ListView(
-            children: _listaItems(snapshot.data,
-                context),
+            children: _listaItems(snapshot.data, context),
           );
         }
       },
@@ -38,9 +34,7 @@ class HomePage extends StatelessWidget {
 
   List<Widget> _listaItems(List<dynamic>? data, BuildContext context) {
     if (data == null) {
-      return [
-        Text('No hay datos disponibles')
-      ];
+      return [Text('No hay datos disponibles')];
     }
 
     final List<Widget> opciones = [];
@@ -51,7 +45,7 @@ class HomePage extends StatelessWidget {
         trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
         onTap: () {
           Navigator.pushNamed(context, opt['ruta']);
-        },  
+        },
       );
       opciones
         ..add(widgetTemp)
--- lib/src/pages/main.dart
+++ lib/src/pages/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
+        // // GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: <Locale>[
